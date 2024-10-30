1299eda5bdaf3796b825732fd67f95339e2adce2
Serhii Burch <serhii_burch@epam.com>
2024-10-30 23:23:47 +0200

    fixed response

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -19,8 +19,9 @@ import java.io.ByteArrayOutputStream;
 @RequestMapping("/api")
 public class Controller {
 
-    @PostMapping("/itext/{text}")
-    public ResponseEntity<ByteArrayOutputStream> itext(@PathVariable String text) {
+    //    @PostMapping(path = "/itext/{text}", produces = "application/pdf")
+    @PostMapping(path = "/itext/{text}", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> itext(@PathVariable String text) {
         log.debug("Received a request for greeting with name: {}", text);
 
         ByteArrayOutputStream pdfStream;
@@ -34,13 +35,16 @@ public class Controller {
         log.debug("PDF '{}' file created: '{}'", outputPath, text);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=itext_test.pdf")
+                .header(HttpHeaders.CONTENT_TYPE, "application/pdf")
+                .header(HttpHeaders.CONTENT_ENCODING, "Binary")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"itext_test.pdf\"")
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"itext_test.pdf\"")
                 .contentType(MediaType.APPLICATION_PDF)
-                .body(pdfStream);
+                .body(pdfStream.toByteArray());
     }
 
     @PostMapping("/pdfbox/{text}")
-    public ResponseEntity<ByteArrayOutputStream> pdfbox(@PathVariable String text) {
+    public ResponseEntity<byte[]> pdfbox(@PathVariable String text) {
         log.debug("Received a request for greeting with name: {}", text);
 
         ByteArrayOutputStream pdfStream;
@@ -54,8 +58,11 @@ public class Controller {
         log.debug("PDF '{}' file created: '{}'", outputPath, text);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=pdfbox_test.pdf")
+                .header(HttpHeaders.CONTENT_TYPE, "application/pdf")
+                .header(HttpHeaders.CONTENT_ENCODING, "Binary")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"pdfbox_test.pdf\"")
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"pdfbox_test.pdf\"")
                 .contentType(MediaType.APPLICATION_PDF)
-                .body(pdfStream);
+                .body(pdfStream.toByteArray());
     }
 }
